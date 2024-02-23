ff0a070decad62848b51439d35c5f61f3d369fec
haifeng <1353120323@qq.com>
2024-02-23 23:12:47 +0800

    update

--- src/invest/stock_md.py
+++ src/invest/stock_md.py
@@ -4,6 +4,7 @@ from .stock import Stock
 from .util import encoded_url
 
 header_temp = """
+更新时间: {}
 ### {}
 * 代码：{}
 * 简介：{}
@@ -39,7 +40,7 @@ def create_md(stock_code):
     stock_info = Stock(stock_code).valuation(17)
 
     filename = "{}{}.md".format(dirs, stock_info.name)
-    header = header_temp.format(stock_info.name, stock_info.code, stock_info.name)
+    header = header_temp.format(current_time, stock_info.name, stock_info.code, stock_info.name)
     chart_url = chart_url_temp.format(
         labels=["21", "22", "23"], data=stock_info.netprofits
     )
